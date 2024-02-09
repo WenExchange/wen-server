7ced8a932e4e4f43be3df12a191dec97d149b5ca
Yourcode <yourcode@Yourcodeui-MacBookPro.local>
2024-02-09 13:57:21 +0900

    :

---
+++ config/plugins.js
@@ -0,0 +1,32 @@
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        region: env('AWS_REGION'),
+        params: {
+            Bucket: env('AWS_BUCKET_NAME'),
+        },
+      },
+      // These parameters could solve issues with ACL public-read access — see [this issue](https://github.com/strapi/strapi/issues/5868) for details
+      actionOptions: {
+        upload: {
+          ACL: null
+        },
+        uploadStream: {
+          ACL: null
+        },
+      }
+    },
+  },
+  "users-permissions": {
+    config: {
+      ratelimit: {
+        interval: 1000 * 60, // 1분
+        max: 100, // 최대 100번
+      },
+    },
+  },
+});
---
+++ connect.sh
@@ -0,0 +1,1 @@
+ssh -i ~/.ssh/ec2-strapi-key-pair.pem ubuntu@43.202.155.8

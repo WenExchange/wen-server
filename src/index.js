47fba24157f6a70a3162ba2671320e0ba38dd918
wenexchange0 <wenexchange0@gmail.com>
2024-02-28 02:38:38 +0900

    :

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 module.exports = {
   /**
@@ -16,26 +16,25 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap( { strapi }) {
-
+  bootstrap({ strapi }) {
     try {
-      const {findBots, checkIsValidTwitterUser, checkIsValidIDiscordUser} = require("./utils/botCheckers")
+      const {
+        findBots,
+        checkIsValidTwitterUser,
+        checkIsValidIDiscordUser,
+      } = require("./utils/botCheckers");
       // 32000
 
-      
-      checkIsValidIDiscordUser({strapi, start: 3000, limit: 7000})
+      checkIsValidIDiscordUser({ strapi, start: 20000, limit: 80000 });
 
       // checkIsValidIDiscordUser({strapi, start: 3000, limit: 1000})
       // checkIsValidIDiscordUser({strapi, start: 52000, limit: 10000})
       // await checkIsValidTwitterUser({strapi,start: 100000, limit: 1})
       // console.log(333, users.length);
-        // findBots(strapi)
-    // deleteBotUsers(strapi)
-
+      // findBots(strapi)
+      // deleteBotUsers(strapi)
     } catch (error) {
-      console.log( error.message)
+      console.log(error.message);
     }
   },
 };
-
-

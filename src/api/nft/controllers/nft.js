b77f0215bcc6c8dd60c1116aad871b85cc324c6a
Avokrypto <avokrypto@gmail.com>
2024-03-13 04:10:08 +0900

    :

--- src/api/nft/controllers/nft.js
+++ src/api/nft/controllers/nft.js
@@ -6,4 +6,30 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::nft.nft');
+module.exports = createCoreController('api::nft.nft', ({ strapi }) => ({
+  
+    async getNFTs(ctx) {
+        console.log(333, "ctx.request.query",ctx.request.query);
+        {
+            try {
+                const {where, orderBy} = ctx.request.query
+                const [entries, count] = await strapi.db.query('api::nft.nft').findWithCount({
+                    ...ctx.request.query,
+                    where: JSON.parse(where),
+                    orderBy: JSON.parse(orderBy)
+                  });
+
+                  return ctx.body = {
+                    entries,
+                    count
+                  }
+
+            } catch (error) {
+                console.error(error.message)
+            }
+        }
+           
+
+        
+    }
+  }));

35319c2647a66cf5712d8e589513f64e5d9411b6
wenexchange0 <wenexchange0@gmail.com>
2024-03-13 21:33:29 +0900

    push push baby

--- src/api/batch-signed-order/services/batch-signed-order.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * batch-signed-order service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::batch-signed-order.batch-signed-order');

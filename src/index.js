45d52ac24fa36392bbf96cd93cb9979443f82e49
Avokrypto <avokrypto@gmail.com>
2024-03-13 14:08:58 +0900

    :

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ module.exports = {
       // findBots(strapi)
       // deleteBotUsers(strapi)
 
-      // createTransferListener();
+      createTransferListener();
     } catch (error) {
       console.log(error.message);
     }

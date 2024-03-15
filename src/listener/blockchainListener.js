4110068ee3b6f96b2afcc59b0cc3357230b66823
Avokrypto <avokrypto@gmail.com>
2024-03-16 07:12:58 +0900

    :

--- src/listener/blockchainListener.js
+++ src/listener/blockchainListener.js
@@ -9,29 +9,14 @@ const {
 } = require("./collectionStats");
 
 //TODO: change it to mainnet
-const jsonRpcProvider = new ethers.providers.JsonRpcProvider(
-  // "https://rpc.ankr.com/blast/c657bef90ad95db61eef20ff757471d11b8de5482613002038a6bf9d8bb84494" // mainnet
-  "https://rpc.ankr.com/blast_testnet_sepolia/c657bef90ad95db61eef20ff757471d11b8de5482613002038a6bf9d8bb84494" // testnet
-);
+const {jsonRpcProvider, NFT_LOG_TYPE} = require("../utils/constants")
 const wenExContractAddress = "0xD75104c9C2aeC1594944c8F3a2858C62DEeaE91b";
 const SELECTOR_fillBatchSignedERC721Order = "0xa4d73041";
 const SELECTOR_fillBatchSignedERC721Orders = "0x149b8ce6";
 
-const LOG_TYPE_SALE = "SALE";
-const LOG_TYPE_TRANSFER = "TRANSFER";
-const LOG_TYPE_LISTING = "LISTING";
-const LOG_TYPE_OFFER = "OFFER";
-const LOG_TYPE_COLLECTION_OFFER = "COLLECTION_OFFER";
-const LOG_TYPE_CANCEL_LISTING = "CANCEL_LISTING";
-const LOG_TYPE_AUTO_CANCEL_LISTING = "AUTO_CANCEL_LISTING"; // 유저가 더이상 BUYER 가 아닌 경우
-const LOG_TYPE_CANCEL_OFFER = "CANCEL_OFFER";
-const LOG_TYPE_MINT = "MINT";
+const {LOG_TYPE_SALE,LOG_TYPE_TRANSFER ,LOG_TYPE_LISTING, LOG_TYPE_OFFER, LOG_TYPE_COLLECTION_OFFER, LOG_TYPE_CANCEL_LISTING, LOG_TYPE_AUTO_CANCEL_LISTING, LOG_TYPE_CANCEL_OFFER, LOG_TYPE_MINT} = NFT_LOG_TYPE
 
-//TODO: update myCollections
-const myCollections = [
-  "0x7E3D4B14E191533B44470889b6d0d36F232de1A3",
-  "0xEFFBE8DFc7B147a59Dd407Efb8b5510804C02236",
-];
+const CollectionCacheManager = require("../cache-managers/CollectionCacheManager")
 
 async function createTransferListener({ strapi }) {
   console.log("it's on");
@@ -46,6 +31,8 @@ async function createTransferListener({ strapi }) {
   jsonRpcProvider.on(filter, async (log, _) => {
     // // exit early if it's not our NFT
     try {
+      const ccm = CollectionCacheManager.getInstance(strapi)
+      const myCollections = ccm.getCollectionAddresses()
       if (!myCollections.includes(log.address)) return;
 
       const transferFrom = `0x${log.topics[1].slice(-40)}`;

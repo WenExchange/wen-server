9751f0b624641eeb728fc3481cf8828c1e42f761
Avokrypto <avokrypto@gmail.com>
2024-03-17 04:46:12 +0900

    :

---
+++ src/cron/start_collelction.js
@@ -0,0 +1,211 @@
+const { NFT_LOG_TYPE }  = require("../utils/constants") 
+
+const dayjs = require("dayjs");
+
+const stats_1h_collection =  async ({ strapi }) => {
+    console.log("[CRON TASK] 1H COLLECTION STATS");
+    try {
+      const colllections = await strapi.db.query("api::collection.collection").findMany();
+      const statUpdatePromises = colllections.map(collection => {
+        const timestamp = dayjs().unix()
+        const collection_id = collection.id
+        const floor_price_1h = collection.floor_price
+       
+        const seconds_1h = 60 * 60
+        return strapi.db.query("api::collection-stat-log.collection-stat-log").findOne({
+            where: {
+              timestamp: {
+                $gte: timestamp - seconds_1h 
+              },
+              collection: {
+                id: {
+                  $eq: collection_id
+                }
+              }
+            },
+            populate: {
+              collection: true
+            }
+          }).then(statLog => {
+          if (statLog) return null
+          return strapi.db.query( "api::nft-trade-log.nft-trade-log").findMany({
+            where: {
+              $and: [
+                {
+                  timestamp: {
+                    $gte: timestamp - seconds_1h 
+                  },
+                },
+                {
+                  type:{
+                    $eq: NFT_LOG_TYPE.LOG_TYPE_SALE
+                  }
+                },
+                {
+                  nft: {
+                    collection: {
+                      id: {
+                        $eq: collection_id
+                      }
+                    }
+                  }
+                }
+              ]
+            },
+            populate: {
+              nft: {
+                collection :true
+              }
+            }
+          }).then(nftTradeLogs => {
+            const volume_1h = nftTradeLogs.reduce((accumulator, currentValue) => {
+              return accumulator + currentValue.price;
+          }, 0);
+          const sale_1h = nftTradeLogs.length
+
+          return {
+            timestamp,
+            collection_id,
+            floor_price_1h,
+            volume_1h,
+            sale_1h,
+            volume_update_info: {
+                prev_volume_total: collection.volume_total,
+                prev_volume_24h: collection.volume_24h,
+                prev_volume_7d: collection.volume_7d
+            }
+          }
+       
+          })
+        })
+        
+        
+
+        
+  
+      })
+      const collectionStatDatas = await Promise.all(statUpdatePromises)
+      const filteredCollectionStateDatas = collectionStatDatas.filter(_ => _ !== null)
+      const collectionStatCreatePromises = filteredCollectionStateDatas.map(collectionStatData => {
+        return strapi.entityService.create("api::collection-stat-log.collection-stat-log", {
+          data: {
+            ...collectionStatData,
+            collection: collectionStatData.collection_id
+          }
+        })
+      })
+
+     /** Create */ 
+      await Promise.all(collectionStatCreatePromises).catch(error=> console.error(error.message))
+
+      /** Update Volume Stats */
+      updateCollectionVolumes({strapi,filteredCollectionStateDatas }).catch(error=> console.error)
+
+    }
+      
+      catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  const updateCollectionVolumes = async ({strapi, filteredCollectionStateDatas}) => {
+      // total volume 추가하기
+
+      try {
+        const seconds_1h = 60 * 60
+        const seconds_24h = seconds_1h * 24
+        const seconds_7d = seconds_24h * 7
+        
+  
+        const updateVolumesPromises = filteredCollectionStateDatas.map(collectionStatData => {
+            
+          const {timestamp,collection_id,volume_update_info, volume_1h  } = collectionStatData
+          return   strapi.db.query("api::collection-stat-log.collection-stat-log").findMany({
+              where: {
+                timestamp: {
+                  $gte: timestamp - seconds_7d +  seconds_1h / 2
+                },
+                collection: {
+                  id: {
+                    $eq: collection_id
+                  }
+                }
+              },
+              populate: {
+                collection: true
+              },
+              offset: 0,
+              limit: 24 * 7,
+              orderBy: {
+                  timestamp: "desc"
+              }
+            }).then(collectionStats => {
+              const volume_7d = collectionStats.reduce((accumulator, currentValue) => {
+                  return accumulator + currentValue.volume_1h;
+              }, 0);
+  
+              const volume_24h = collectionStats.slice(0, 24).reduce((acc, item) => acc + item.volume_1h, 0);
+
+     
+     
+              let change_24h = 0
+              const collectionStats_24h = collectionStats.slice(0, 24)
+              if (Array.isArray(collectionStats_24h && collectionStats_24h > 0)) {
+                const currentFloorPrice = collectionStats_24h[0].floor_price_1h
+                const pastFloorPrice = collectionStats_24h[collectionStats.length - 1].floor_price_1h
+                change_24h = calculatePriceChangeRate(currentFloorPrice, pastFloorPrice) 
+              }
+
+              let change_7d = 0
+              if (Array.isArray(collectionStats && collectionStats.length > 0)) {
+                const _currentFloorPrice = collectionStats[0].floor_price_1h
+                const _pastFloorPrice = collectionStats[collectionStats.length - 1].floor_price_1h
+                change_7d = calculatePriceChangeRate(_currentFloorPrice, _pastFloorPrice) 
+              }
+
+            
+
+
+              return {
+                  collection_id,
+                  volume_7d,
+                  volume_24h,
+                  volume_total: volume_update_info.prev_volume_total + volume_1h,
+                  change_24h,change_7d
+              }
+  
+            })
+        })
+  
+        const collectionUpdateDatas = await Promise.all(updateVolumesPromises)
+        for (let i = 0; i < collectionUpdateDatas.length; i++) {
+            const collectionUpdateData = collectionUpdateDatas[i];
+            
+            await strapi.entityService.update("api::collection.collection",collectionUpdateData.collection_id,{
+                data: {
+                 ...collectionUpdateData
+                }
+              })
+            
+        }
+      } catch (error) {
+          console.error(error.message)
+      }
+   
+      
+
+      
+  }
+  
+  function calculatePriceChangeRate(currentFloorPrice, pastFloorPrice) {
+    const changeRate = ((currentFloorPrice - pastFloorPrice) / pastFloorPrice) * 100;
+    return Number(changeRate.toFixed(2));
+}
+
+
+
+
+
+  module.exports = {
+    stats_1h_collection
+  }

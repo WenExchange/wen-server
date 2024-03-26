4b619934627a678fac4aa4e9d118c0ac1da180fd
Avokrypto <avokrypto@gmail.com>
2024-03-26 19:45:24 +0900

    :

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ module.exports = {
       // createTransferListener({ strapi });
       // const ccm = CollectionCacheManager.getInstance(strapi);
       console.log(333, "process.env.SERVER_TYPE",process.env.SERVER_TYPE, process.env.SERVER_TYPE === SERVER_TYPE.API );
-      const collection = await strapi.db.query("api:collection.collection").findOne({
+      const collection = await strapi.db.query("api::collection.collection").findOne({
         where: {
           id: 119
         }

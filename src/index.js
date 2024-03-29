d236f540a97c5b775d155fb3a34dc0186c3e9ba8
Avokrypto <avokrypto@gmail.com>
2024-03-29 20:17:35 +0900

    :

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const dayjs = require("dayjs");
 var utc = require("dayjs/plugin/utc");
 var timezone = require("dayjs/plugin/timezone"); // dependent on utc plugin
 const { listingCollectionScript } = require("./utils/listing-script");
+const { updateEarlyUsers } = require("./utils/botCheckers");
 dayjs.extend(utc);
 dayjs.extend(timezone);
 module.exports = {
@@ -21,6 +22,7 @@ module.exports = {
   async bootstrap({ strapi }) {
     try {
       // listingCollectionScript({address: "0xc904e6115f011fC530ea756A673E0c0eD0334680", strapi})
+      updateEarlyUsers({strapi})
       const isBOTServer = process.env.SERVER_TYPE === SERVER_TYPE.BOT
       if (isBOTServer) {
         createTransferListener({ strapi }).catch(e => { 

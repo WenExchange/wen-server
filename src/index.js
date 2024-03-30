7840d3e8726e1b75565287b0f0250195072faa48
Avokrypto <avokrypto@gmail.com>
2024-03-30 23:39:14 +0900

    :

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ const dayjs = require("dayjs");
 var utc = require("dayjs/plugin/utc");
 var timezone = require("dayjs/plugin/timezone"); // dependent on utc plugin
 const { listingCollectionScript } = require("./utils/listing-script");
-const { updateEarlyUsers, updatePastEarlyUsers, checkBotUsers, checkIsValidTwitterUser } = require("./utils/botCheckers");
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
@@ -27,13 +26,6 @@ module.exports = {
   async bootstrap({ strapi }) {
     try {
       // listingCollectionScript({address: "0xc904e6115f011fC530ea756A673E0c0eD0334680", strapi})
-      // updateEarlyUsers({strapi})
-      // updatePastEarlyUsers({strapi})
-      // checkBotUsers({strapi})
-      // checkIsValidTwitterUser({strapi, user: {
-      //   twitter_id: "1455205839925485571",
-      //   twitter_name: "Airdroop18"
-      // }})
       const isBOTServer = process.env.SERVER_TYPE === SERVER_TYPE.BOT
       if (isBOTServer) {
         createTransferListener({ strapi }).catch((e) => {

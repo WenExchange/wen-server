1fd5cd51ba912e46c62f0c9fba77cbfb76577cda
Avokrypto <avokrypto@gmail.com>
2024-04-08 17:18:07 +0900

    :

--- src/api/collection/routes/collection.js
+++ src/api/collection/routes/collection.js
@@ -6,4 +6,10 @@
 
 const { createCoreRouter } = require("@strapi/strapi").factories;
 
-module.exports = createCoreRouter("api::collection.collection");
+module.exports = createCoreRouter("api::collection.collection", {
+    // config: {
+    //     find: {
+    //       middlewares: ["api::collection.cache-logger"],
+    //     },
+    //   },
+});

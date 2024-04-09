a3c1783c90b84827a9042befe54ca622a07420af
Avokrypto <101148323+Avokrypto@users.noreply.github.com>
2024-04-09 23:35:19 +0900

    :

--- src/api/collection/controllers/collection.js
+++ src/api/collection/controllers/collection.js
@@ -6,4 +6,44 @@
 
 const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController("api::collection.collection");
+module.exports = createCoreController("api::collection.collection",
+({ strapi }) => ({
+
+    async getFindOneCollection(ctx) {
+      {
+        try {
+          const { slug } = ctx.request.query;
+
+          const collection  = await strapi.db
+            .query("api::collection.collection")
+            .findOne({
+              where: {
+                $and: [
+                    {
+                        slug
+                    },
+                    {
+                        publishedAt: {
+                            $notNull: true
+                        }
+                    }
+                ]
+              },
+              populate: {
+                nfts: {
+                    sell_order:true
+                }
+              }
+            });
+            if (!collection) throw new Error("Not found")
+
+
+            return ctx.body = collection
+        } catch (error) {
+            console.error(error.message)
+        }
+    }
+    }
+  })
+
+);

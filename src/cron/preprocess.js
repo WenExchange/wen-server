202bd6b589274b6d27f2b800480814ba7ca45600
wenexchange0 <wenexchange0@gmail.com>
2024-05-13 18:03:20 +0900

    :

--- src/cron/preprocess.js
+++ src/cron/preprocess.js
@@ -104,67 +104,6 @@ const preprocess_mint_second = async ({ strapi }) => {
   }
 };
 
-const deleteBlacklistOnPreprocess = async ({ strapi, isIterateReverse = false }) => {
-  const blacklist = ["0x0c21c610acc756c9b1e157ac90a3e928e5b764a4","0x338BCe2590495B6DE6a7D7aC8514Ad73E7Be0FFB", "0x0AAADCf421A3143E5cB2dDB8452c03ae595B0734", "0xe91a42e3078c6ad358417299e4300683de87f971", "0x65621a6a2cdB2180d3fF89D5dD28b19BB7Dd200a", "0x73A0469348BcD7AAF70D9E34BBFa794deF56081F"]
-  const preprocesses = await strapi.db.query("api::preprocess.preprocess").findMany({
-    where: {
-      $or: blacklist.map(contract_address => {
-        return {
-          nft: {
-            collection: {
-              contract_address
-            }
-          }
-        }
-      })
-
-
-    }
-  })
-
-  console.log(`will delete processes ${preprocesses.length}`);
-
-  const unit = 5
-  for (let i = 0; i < preprocesses.length / unit; i++) {
-    // 0 1 2 3 4
-
-    const preprocess = preprocesses[i];
-    console.log(i);
-    await strapi.db.query("api::preprocess.preprocess").delete({
-      where: {
-        id: preprocess.id
-      }
-    })
-
-  }
-
-
-  if (!isIterateReverse) {
-    for (let i = 0; i < preprocesses.length; i++) {
-      const preprocess = preprocesses[i];
-      console.log(i);
-      await strapi.db.query("api::preprocess.preprocess").delete({
-        where: {
-          id: preprocess.id
-        }
-      })
-
-    }
-  } else {
-    for (let i = 0; i < preprocesses.length; i++) {
-      const preprocess = preprocesses[preprocesses.length - 1 - i];
-      console.log(i);
-      await strapi.db.query("api::preprocess.preprocess").delete({
-        where: {
-          id: preprocess.id
-        }
-      })
-
-    }
-  }
-
-}
-
 const bulkDeleteBlacklistOnPreprocess = async ({ strapi }) => {
   const blacklist = ["0x0c21c610acc756c9b1e157ac90a3e928e5b764a4","0x338BCe2590495B6DE6a7D7aC8514Ad73E7Be0FFB", "0x0AAADCf421A3143E5cB2dDB8452c03ae595B0734", "0xe91a42e3078c6ad358417299e4300683de87f971", "0x65621a6a2cdB2180d3fF89D5dD28b19BB7Dd200a", "0x73A0469348BcD7AAF70D9E34BBFa794deF56081F"]
   const preprocesses = await strapi.db.query("api::preprocess.preprocess").findMany({
@@ -278,7 +217,7 @@ const bulkDeleteBlacklistNFT = async ({ strapi }) => {
 module.exports = {
   preprocess_mint,
   preprocess_mint_second,
-  deleteBlacklistOnPreprocess, bulkDeleteBlacklistOnPreprocess,
+  bulkDeleteBlacklistOnPreprocess,
   bulkDeleteBlacklistNFT
 
 };
